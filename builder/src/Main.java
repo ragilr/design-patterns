a3874de090fc0699aa8b6415dd66164a70101318
ragil-kv <ragil@keyvalue.systems>
2023-07-22 11:03:17 +0530

    Refactor adapter module location

--- builder/src/Main.java
+++ builder/src/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
         User user = new User.UserBuilder("Parvathi", "Ragil").age(3).phone("987654321").address("Calicut").build();
         System.out.println(user);
-//        List l = Arrays.asList([1,2,3]);
+//        List l = Arrays.asList([1,2,3]
     }
 }
